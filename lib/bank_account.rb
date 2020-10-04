3e980a8323f1c4bb370d6f9c216532f0901f9789
Pbsmith82 <pbsmith82@gmail.com>
2020-10-04 09:57:44 -0400

    Bank Transfers

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
 class BankAccount
+    attr_accessor :status, :balance, :deposit, :withdraw
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        @balance = @balance + amount
+    end
+
+    def withdraw(amount)
+        @balance = @balance - amount
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+            return true
+        else
+            return false
+        end
+    end
+
+    def balance
+        @balance
+    end
+
+    def close_account
+        @balance = 0
+        @status = "closed"
+    end
 
 end
